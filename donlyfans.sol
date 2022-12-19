6d9a084cfb1e520c9fb699e199eee6ce1facf976
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2022-12-19 18:52:14 +0000

    first basic contract that allows CC to set their price and subscribers to subscribe to them for that price

---
+++ donlyfans.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import {PullPayment} from "@openzeppelin/contracts/security/PullPayment.sol";
+
+
+struct Creator{
+    address CCaddress;
+    uint256 price;
+    address[] subscribers; // list of subscribers
+    bool isCreator;
+
+}
+
+contract dOnlyFans is PullPayment {
+
+    error InsufficientFunds();
+    error CreatorDoesNotExist();
+
+    address[] subs;
+
+    mapping(address => Creator) public creators;
+
+    function createProfile(uint256 price) public returns(bool) {
+        if ((creators[msg.sender].CCaddress)== msg.sender){ // the creator profile already exists
+            return false;
+        }
+        address[] storage _subs = subs;
+        creators[msg.sender] = Creator(msg.sender, price, _subs, true);
+        return true;
+    }
+
+
+
+    function subscribe(address creatorAddress) external payable {
+        Creator storage creator = creators[creatorAddress];
+        if (!creator.isCreator) revert CreatorDoesNotExist();
+        if (msg.value < creator.price) revert InsufficientFunds();
+        creator.subscribers.push(msg.sender);
+        _asyncTransfer(creatorAddress, msg.value);
+
+
+    }
+
+    function getSuscribers(address creatorAddress) public view returns(address[] memory) {
+        return creators[creatorAddress].subscribers;
+    }
+
+    function withdrawFunds() public {
+        withdrawPayments(payable(msg.sender));
+    }
+
+
+    // function unsuscribe(address creatorAddress){
+    //     Creator memory creator = creators[creatorAddress];
+
+    // }
+}
