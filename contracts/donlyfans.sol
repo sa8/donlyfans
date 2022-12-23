7c1b4a40a16899540b34e2dee4cabca03a2a35b9
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2022-12-23 21:26:43 +0000

    hardhat testing

--- contracts/donlyfans.sol
+++ contracts/donlyfans.sol
@@ -4,26 +4,25 @@ pragma solidity ^0.8.0;
 
 import {PullPayment} from "@openzeppelin/contracts/security/PullPayment.sol";
 
-
-struct Creator{
+struct Creator {
     address CCaddress;
     uint256 price;
     address[] subscribers; // list of subscribers
     bool isCreator;
-
 }
 
 contract dOnlyFans is PullPayment {
-
     error InsufficientFunds();
     error CreatorDoesNotExist();
+    error NotSubscriber();
 
     address[] subs;
 
     mapping(address => Creator) public creators;
 
-    function createProfile(uint256 price) public returns(bool) {
-        if ((creators[msg.sender].CCaddress)== msg.sender){ // the creator profile already exists
+    function createProfile(uint256 price) public returns (bool) {
+        if ((creators[msg.sender].CCaddress) == msg.sender) {
+            // the creator profile already exists
             return false;
         }
         address[] storage _subs = subs;
@@ -31,19 +30,17 @@ contract dOnlyFans is PullPayment {
         return true;
     }
 
-
-
     function subscribe(address creatorAddress) external payable {
         Creator storage creator = creators[creatorAddress];
         if (!creator.isCreator) revert CreatorDoesNotExist();
         if (msg.value < creator.price) revert InsufficientFunds();
         creator.subscribers.push(msg.sender);
         _asyncTransfer(creatorAddress, msg.value);
-
-
     }
 
-    function getSuscribers(address creatorAddress) public view returns(address[] memory) {
+    function getSubscribers(
+        address creatorAddress
+    ) public view returns (address[] memory) {
         return creators[creatorAddress].subscribers;
     }
 
@@ -51,9 +48,15 @@ contract dOnlyFans is PullPayment {
         withdrawPayments(payable(msg.sender));
     }
 
-
-    // function unsuscribe(address creatorAddress){
-    //     Creator memory creator = creators[creatorAddress];
-
-    // }
+    function unsubscribe(address creatorAddress) public {
+        Creator memory creator = creators[creatorAddress];
+        if (!creator.isCreator) revert CreatorDoesNotExist();
+        for (uint i; i < creator.subscribers.length; i++) {
+            if (msg.sender == creator.subscribers[i]) {
+                delete creator.subscribers[i];
+                return;
+            }
+        }
+        revert NotSubscriber();
+    }
 }
---
+++ test/donlyfans.js
@@ -0,0 +1,55 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("dOnlyFans contract", function () {
+  async function deployDOnlyFansFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const dOnlyFans = await ethers.getContractFactory("dOnlyFans");
+
+    const hardhatDOnlyFans = await dOnlyFans.deploy();
+
+    await hardhatDOnlyFans.deployed();
+
+    const createProfile = await hardhatDOnlyFans.createProfile(2);
+
+    const firstSubscription = await hardhatDOnlyFans
+      .connect(addr1)
+      .subscribe(owner.address, {
+        value: ethers.utils.parseEther("2.0"),
+      });
+
+    // Fixtures can return anything you consider useful for your tests
+    return { dOnlyFans, hardhatDOnlyFans, owner, addr1, addr2 };
+  }
+  it("Verify subscription", async function () {
+    const { hardhatDOnlyFans, owner, addr1 } = await loadFixture(
+      deployDOnlyFansFixture
+    );
+    //console.log(createProfile);
+    const [subs] = await hardhatDOnlyFans.getSubscribers(owner.address);
+    console.log(subs);
+    console.log(addr1.address);
+    expect(subs).to.equal(addr1.address);
+  });
+
+  it("Unsubscribing", async function () {
+    const { hardhatDOnlyFans, owner, addr1, addr2 } = await loadFixture(
+      deployDOnlyFansFixture
+    );
+    const secondSubscription = await hardhatDOnlyFans
+      .connect(addr2)
+      .subscribe(owner.address, {
+        value: ethers.utils.parseEther("2.0"),
+      });
+
+    const unsub = await hardhatDOnlyFans
+      .connect(addr1)
+      .unsubscribe(owner.address);
+
+    const subs = await hardhatDOnlyFans.getSubscribers(owner.address);
+    console.log(subs);
+    //expect(subs).to.equal(addr2.address);
+  });
+});
